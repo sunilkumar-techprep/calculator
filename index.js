c88ea464c70ed4782ea75efac30c54a12636e9c4
sunilkumar-techprep <117669776+sunilkumar-techprep@users.noreply.github.com>
2022-11-07 11:51:14 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+
+ let num=" ";
+function number (num){
+    var result =document.getElementById("input")
+    if( num === '.' && result.value.includes('.')) return
+    if( num === '+' && result.value.includes('+')) return
+    if( num === '-' && result.value.includes('-')) return
+    if( num === '*' && result.value.includes('*')) return
+   if( num === '/' && result.value.includes('/')) return
+   result.value += num;
+    }
+function result (){
+    var result = document.getElementById("input")
+    result.value =eval(result.value)
+}
+ function clearResult(){
+    var result = document.getElementById("input")
+    result.value =" "
+ }
+ function deleteChar(){
+    var number=document.getElementById("input")
+    var remove= number.value;
+    remove=remove.slice(0,-1)
+    number.value=remove
+ } 
